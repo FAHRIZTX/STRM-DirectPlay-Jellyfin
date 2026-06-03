13c35eb447ad7254fb829510d29cfde61ca3d3d7
Muhammad Fahri <mfahri.dev@gmail.com>
2026-06-04 02:50:58 +0700

    v1.0.9

--- Jellyfin.Plugin.StrmDirectPlay/StrmMediaSourceManager.cs
+++ Jellyfin.Plugin.StrmDirectPlay/StrmMediaSourceManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Jellyfin.Data.Entities;
+using Jellyfin.Database.Implementations.Entities;
 using Jellyfin.Plugin.StrmDirectPlay.Core;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Library;
